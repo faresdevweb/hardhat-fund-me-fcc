aac69afe693e1421c110e1463dd0aa319cbcd36e
faresdevweb <fares.meier@hotmail.com>
2023-09-01 09:22:12 +0200

    removed console.log

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -9,17 +9,10 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
   const { deploy, log } = deployments;
   log("DEPLOYING FUNDME CONTRACT");
   log("-------------------------------------------------------");
-  console.log("DEPLOYMENTS : ", deploy);
-  console.log("LOGS : ", log);
 
   const { deployer } = await getNamedAccounts();
 
-  console.log("DEPLOYER : ", deployer);
-
   const chainId = network.config.chainId;
-
-  console.log("CHAIN ID : ", chainId);
-
   let ethUsdPriceFeedAddress;
 
   if (developmentChains.includes(network.name)) {
@@ -28,8 +21,6 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
   } else {
     ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeedAddress"];
   }
-
-  console.log("ETH/USD ADDRESS: ", ethUsdPriceFeedAddress);
   // when going for localhost or hadhat network we want to use a mock
   const args = [ethUsdPriceFeedAddress];
   const FundMe = await deploy("FundMe", {
